96aaaaf6fd50a795f82931f0b136da12b2f2a317
ElemartA <info88881@gmail.com>
2021-02-09 19:55:54 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const arrayСombined = [];
+    bracketsConfig.forEach(arrayElement => arrayСombined.push(arrayElement.join('')));
+    const newStr = (str) => {
+        let strComparison = str;
+        for (let i = 0; i < arrayСombined.length; i++) {
+            strComparison = strComparison.replace(arrayСombined[i], '')
+        }
+        return str === strComparison ? strComparison : newStr(strComparison)
+
+    }
+    const result = newStr(str);
+    return result === '' ? true : false;
 }
